6d25a21e41657e3ab705b435bc0d8f55df8ebed8
kenny <kenny.lozeau@gmail.com>
2020-02-19 20:00:16 -0500

    Build Users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,23 @@
 class Api::UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :name, :password)
+  end
 end
