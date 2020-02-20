b82495c1acf5e5d6f0e9fefb6b087daa5e2a854d
kenny <kenny.lozeau@gmail.com>
2020-02-19 20:04:47 -0500

    Build Sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,26 @@
 class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(user_params[:email], user_params[:password])
+
+    if @user
+      login(@user)
+      render "pi/users/show"
+    else
+      render json: ["Wrong Username/Password Combo"], status: 401
+  end
+
+  def destroy
+    if current_user
+      logout
+      render json: ["Logged out successfully"]
+    else
+      render json: ["You are not logged in"], status: 404
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
