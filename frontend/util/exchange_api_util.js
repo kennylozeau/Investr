5f75dcbf11269f38661cf77725bf070920b08a48
kenny <kenny.lozeau@gmail.com>
2020-02-25 18:26:39 -0500

    Change IEX call to live from sandbox

--- frontend/util/exchange_api_util.js
+++ frontend/util/exchange_api_util.js
@@ -1,12 +1,14 @@
 export const fetchStock = symbol => {
   return $.ajax({
-    url: `https://sandbox.iexapis.com/stable/stock/${symbol.toLowerCase()}/quote?token=Tpk_3db45ef3678f4a18b23386fbcacce73c`
+    // url: `https://sandbox.iexapis.com/stable/stock/${symbol.toLowerCase()}/quote?token=Tpk_3db45ef3678f4a18b23386fbcacce73c`
+    url: `https://cloud.iexapis.com/stable/stock/${symbol.toLowerCase()}/quote?token=pk_c9ecf2fb9f674aa3a3f87c3a417e8210`
   });
 };
 
 export const fetchAllStocks = symbols => {
   let symString = symbols.map(sym => sym.toLowerCase()).join(",");
   return $.ajax({
-    url: `https://sandbox.iexapis.com/stable/stock/market/batch?symbols=${symString}&types=quote&token=Tpk_3db45ef3678f4a18b23386fbcacce73c`
+    // url: `https://sandbox.iexapis.com/stable/stock/market/batch?symbols=${symString}&types=quote&token=Tpk_3db45ef3678f4a18b23386fbcacce73c`
+    url: `https://cloud.iexapis.com/stable/stock/market/batch?symbols=${symString}&types=quote&token=pk_c9ecf2fb9f674aa3a3f87c3a417e8210`
   });
 };
